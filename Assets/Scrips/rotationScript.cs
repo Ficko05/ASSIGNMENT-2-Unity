1bf4d4ecbd5da5b0d19f065fd7acaf8bcaa11a97
Ficko <you@example.com>
2019-02-17 18:04:06 +0100

    assigment 3 update

---
+++ Assets/Scrips/rotationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationScript : MonoBehaviour
+{
+
+
+    
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+        transform.Rotate(Vector3.right * Time.deltaTime);
+
+        
+        transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
